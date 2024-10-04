b80be3d7e3b78f386dc025266baaf00577978b53
Иван Гриненко <4l1kku@gmail.com>
2024-10-04 23:41:42 +0300

    add download csv buttons

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <div v-for="name in tables" :key="name">
+      <form action="/api/get_csv_table" method="post">
+        <input type="text" name="table_name" :value="name" hidden>
+        <button type="submit">Скачать {{name}}</button>
+      </form>
+    </div>
+    <ul>
+      <li v-for="user in users" :key="user">{{ user }}</li>
+    </ul>
   </div>
 </template>
 
@@ -9,15 +17,21 @@ export default {
   data() {
     return {
       message: "",
+      tables: ['location_types', 'locations', 'air_measurments', 'country_codes_adjacent',
+        'areas_and_densities', 'crop_production', 'populations', 'blocks_and_roads',
+        'energy_consumption', 'trends', 'co2_emission'
+      ]
     };
   },
 
   mounted() {
+    /*
     fetch("/api")
       .then((response) => response.text())
       .then((data) => {
         this.message = data;
       });
+      */
   },
 };
 </script>
